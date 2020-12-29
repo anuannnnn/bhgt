9f1b0f61537f98b205fe53e4c399e32a933fe033
kju <ancyrajan7382@gmail.com>
2020-12-29 20:24:35 +0530

    first commit:first version

---
+++ ajax.js
@@ -0,0 +1,22 @@
+function ajax()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function()
+    {
+        if(this.readyState==4&&this.status==200)
+        {
+            var response = JSON.parse(this.responseText);
+            var output ="";
+            for(var i=0;i<response.length;i++){
+                
+           output += "<li>"+"<input type='checkbox' class='chk' >"+response[i].title+"</td>";
+         
+
+           
+    }
+        document.getElementById("demo").innerHTML=output;
+    }
+}
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/posts",true);
+    xhttp.send();
+}
